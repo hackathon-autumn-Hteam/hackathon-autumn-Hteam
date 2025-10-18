cbdd969136d1d0e8abb6cd413bceea069dd76b9f
blue-chat-tea <yktanimoto@gmail.com>
2025-10-18 19:41:23 +0900

    [add] message機能の関数を追加

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -19,7 +19,31 @@ app.permanent_session_lifetime = timedelta(days=SESSION_DAYS)
 
 
 # TODO(rootさん): メッセージ用の関数定義
+# TODO:メッセージ一覧ページの表示
+@app.route('/channels/<channel_id>/messages', methods=['GET'])
+def messages_view():
+    #
+    return 'message view'
 
+# TODO:メッセージの投稿
+@app.route('/channels/<channel_id>/messages', methods=['POST'])
+def create_message():
+    return 'create message'
+
+# TODO:メッセージの編集
+@app.route('/channels/<channel_id>/messages/<message_id>', methods=['PUT'])
+def update_message():
+    return 'update message'
+
+# TODO:メッセージの削除(追加機能)
+@app.route('/channels/<channel_id>/messages/<message_id>', methods=['DELETE'])
+def delete_message():
+    return 'delete message'
+
+# TODO:メッセージにお花(いいね)を押す(追加機能)
+@app.route('/channels/<channel_id>/messages/<message_id>/flowers', methods=['POST'])
+def send_flower():
+    return 'send flower'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
