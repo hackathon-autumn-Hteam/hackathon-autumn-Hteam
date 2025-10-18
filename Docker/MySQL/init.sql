5be08b1d4e8b9cf397bef972484ece1c29f514a5
Haru1228T <haru1228tech@gmail.com>
2025-10-18 09:58:23 +0900

    ユーザーテーブルと都道府県テーブルを定義

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -7,12 +7,18 @@ CREATE DATABASE chatapp;
 USE chatapp
 GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
 
-# TODO(はるか): ユーザーテーブルの定義
+CREATE TABLE prefectures (
+    prefecture_id INT AUTO_INCREMENT PRIMARY KEY,
+    prefecture_name VARCHAR(255) NOT NULL
+)
+
 CREATE TABLE users (
-    uid VARCHAR(255) PRIMARY KEY,
-    user_name VARCHAR(255) UNIQUE NOT NULL,
+    user_id VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    prefecture_id INT NOT NULL,
+    FOREIGN KEY (prefecture_id) REFERENCES prefectures(prefecture_id) ON DELETE RESTRICT
 );
 
 # TODO(うっちーさん): チャンネルテーブルの定義
@@ -35,7 +41,13 @@ CREATE TABLE messages (
     FOREIGN KEY (cid) REFERENCES channels(id) ON DELETE CASCADE
 );
 
-# TODO(はるか): 都道府県テーブルの定義
-
-
-# TODO(はるか): 都道府県の初期データを定義
+INSERT INTO prefectures (prefecture_name)
+VALUES
+("北海道"),
+("青森県"), ("岩手県"), ("宮城県"), ("秋田県"), ("山形県"), ("福島県"),
+("茨城県"), ("栃木県"), ("群馬県"), ("埼玉県"), ("千葉県"), ("東京都"), ("神奈川県"),
+("新潟県"), ("富山県"), ("石川県"), ("福井県"), ("山梨県"), ("長野県"),("岐阜県"), ("静岡県"), ("愛知県"),
+("三重県"),("滋賀県"), ("京都府"), ("大阪府"), ("兵庫県"), ("奈良県"), ("和歌山県"),
+("鳥取県"), ("島根県"), ("岡山県"), ("広島県"), ("山口県"),
+("徳島県"), ("香川県"), ("愛媛県"), ("高知県"),
+("福岡県"), ("佐賀県"), ("長崎県"), ("熊本県"), ("大分県"), ("宮崎県"), ("鹿児島県"), ("沖縄県");
