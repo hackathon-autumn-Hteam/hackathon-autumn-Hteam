a7910a0042d6147c6ce36452dbdd6ce6fbf28438
aya <rare.tomo038@gmail.com>
2025-10-26 21:50:14 +0900

    チャンネル表示、作成、更新、削除を作成しました

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,5 +1,6 @@
 import pymysql
 from util.DB import DB
+from flask import abort
 
 
 # 初期起動時にコネクションプールを作成し接続を確立
@@ -14,6 +15,108 @@ class User:
 
 
 # TODO(うっちーさん): チャンネルクラスを定義
+#チャンネル一覧ページの表示
+    @classmethod
+    def get_all(cls):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM channels;"
+                
+                #データベース側で並び替えを指定するなら（app.pyのreverse()削除）
+                #sql = SELECT * FROM ORDER BY channel_id DESC
+                                
+                cur.execute(sql)
+                channels = cur.fetchall()
+                return channels
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')  #HTMLには表示されないもの　flash? 仕組み調べる
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+#チャンネルの作成
+class Channel:
+    @classmethod
+    def create(cls,user_id, channel_name, description):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO channels(user_id, channel_name, description) VALUES (%s, %s, %s);" 
+                cur.execute(sql, (user_id, channel_name, description,))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+          
+#チャンネルの更新
+    """チャンネルIDの検索条件"""
+    @classmethod
+    def find_by_channel_id(cls, channel_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM channels WHERE channel_id=%s;"
+                cur.execute(sql, (channel_id,))
+                channel = cur.fetchone()
+                return channel
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+            
+    """チャンネルの名前の検索条件"""
+    @classmethod
+    def find_by_channel_name(cls, channel_name):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM channels WHERE channel_name=%s;"
+                cur.execute(sql,(channel_name,))
+                channel = cur.fetchone()
+                return channel
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+            
+    @classmethod
+    def update(cls, user_id, channel_name, description, channel_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "UPDATE channels SET user_id=%s, channel_name=%s, description=%s WHERE channel_id=%s;"
+                cur.execute(sql, (user_id, channel_name, description, channel_id,))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+            
+            
+#チャンネルの削除
+    @classmethod
+    def delete(cls, channel_id):
+        conn =db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql ="DELETE FROM channels WHERE channel_id=%s;"
+                cur.execute(sql, (channel_id))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しました:{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+    
 
 
 # TODO(rootさん): メッセージクラスを定義
