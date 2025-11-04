051fcaafbdda8691ea85620cdd0d77a5c49e8cd6
blue-chat-tea <yktanimoto@gmail.com>
2025-11-04 22:23:19 +0900

    [update] 編集ボタンを押した時の動作の追加

--- ChatApp/static/js/channels/update-channel.js
+++ ChatApp/static/js/channels/update-channel.js
@@ -1,16 +1,18 @@
 // チャンネルを編集するモーダルの制御
 
-// モーダルウィンドウを「開く」ボタン
+// モーダルウィンドウを「開く」ボタン @messages.html
 const updateButton = document.getElementById("update-channel-button");
 
-// モーダルウィンドウを「閉じる」ボタン。
+// モーダルウィンドウを「閉じる」ボタン @update-channel.html
 const updatePageButtonClose = document.getElementById("close-modal-button");
 
-// 「編集する」ボタン
-
-// モーダル。IDは後で確認@update-channel.html。
+// モーダル @update-channel.html
 const updateChannelModal = document.getElementById("update-channel-modal");
-// マスク(モーダルの背景をマスクする)
+
+// 「編集する」ボタン @update-channel.html
+const editChannelDescription = document.getElementById(
+  "edit-channel-description-button"
+);
 
 // モーダルが存在するページのみ（uidとチャンネルidが同じ時のみ）
 if (updateChannelModal) {
@@ -26,23 +28,23 @@ if (updateChannelModal) {
     updateChannelModal.style.display = "none";
   });
 
+  // チャンネル編集モーダル内の「編集する」ボタンが押された時に情報を送信する
+  editChannelDescription.addEventListener("click", () => {
+    const newChannelDescription =
+      document.updateChannelForm.channelDescription.value;
+    if (newChannelDescription !== "") {
+      document.updateChannelForm.submit();
+    }
+    updateChannelModal.style.display = "flex";
+  });
+
   // TODO(rootさん): 画面のどこかが押された時に、モーダルを非表示にする
   addEventListener("click", (e) => {
     if (e.target == updateChannelModal) {
       updateChannelModal.style.display = "none";
     }
   });
-
-  // TODO(root):チャンネル編集モーダル内の「編集する」ボタンが押された時にモーダルを非表示にする
 } else {
+  // デバッグ用
   console.log("モーダルが見つかりません");
-}
-
-// update-channel-modalが表示されている時に Ctrl/Command + Enter で送信
-function sendUpdateForm() {
-  const newChannelTitle = document.updateChannelForm.channelTitle.value;
-
-  if (newChannelTitle !== "") {
-    document.updateChannelForm.submit();
-  }
 }
