b3128220fcc3d84572f9f66f45e63d2ca3d7612c
191 <kawauso.1288@gmail.com>
2025-11-15 14:29:27 +0900

    logout modal

--- ChatApp/static/js/logout.js
+++ ChatApp/static/js/logout.js
@@ -7,13 +7,3 @@ var closeSpan = document.getElementsByClassName("close")[0];
 openBtn.onclick = function() {
     modal.style.display = "block";
 }
-
-closeSpan.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
