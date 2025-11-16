45fa85e2c21b4a3b4e56e989b35b384466c41858
はるか@58期 <haru1228tech@gmail.com>
2025-11-16 09:29:19 +0900

    send_flowerが重複していたため片方を削除

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -346,22 +346,6 @@ def update_message(channel_id, message_id):
     return redirect(f"/channels/{channel_id}/messages")
 
 
-@app.route("/channels/<channel_id>/messages/<message_id>/flowers", methods=["POST"])
-def send_flower(channel_id, message_id):
-    """お花を送る処理
-
-    Args:
-        channel_id (int): チャンネルID
-        message_id (int): メッセージID
-
-    Returns:
-        flask.Response: リダイレクト先のHTTPレスポンス。
-            メッセージ一覧ページ(messages_view)へのリダイレクト。
-    """
-    Message.send_flower(message_id)
-    return redirect(f"/channels/{channel_id}/messages")
-
-
 # TODO:メッセージの削除(追加機能)
 @app.route("/channels/<channel_id>/messages/<message_id>", methods=["DELETE"])
 def delete_message(channel_id, message_id):
@@ -397,10 +381,20 @@ def delete_message(channel_id, message_id):
     return redirect(f"/channels/{channel_id}/messages")
 
 
-# TODO:メッセージにお花(いいね)を押す(追加機能)
 @app.route("/channels/<channel_id>/messages/<message_id>/flowers", methods=["POST"])
-def send_flower():
-    return "send flower"
+def send_flower(channel_id, message_id):
+    """お花を送る処理
+
+    Args:
+        channel_id (int): チャンネルID
+        message_id (int): メッセージID
+
+    Returns:
+        flask.Response: リダイレクト先のHTTPレスポンス。
+            メッセージ一覧ページ(messages_view)へのリダイレクト。
+    """
+    Message.send_flower(message_id)
+    return redirect(f"/channels/{channel_id}/messages")
 
 
 if __name__ == "__main__":
