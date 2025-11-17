2d43bb0fdcce9857acd750bd9872be192c0deba1
aya <rare.tomo038@gmail.com>
2025-11-17 22:23:27 +0900

    レビューの修正をしました

--- ChatApp/static/js/channels/create-channel.js
+++ ChatApp/static/js/channels/create-channel.js
@@ -1,25 +1,24 @@
 // チャンネルを新規作成するモーダルの制御
-export const initCreateChannelModal = () => {
-  const createChannelModal = document.getElementById("create-channel-modal");
-  const createChannelButton = document.getElementById("create-channel-button");
-  const closeButton = document.getElementById(
-    "create-channel-modal-close-button"
-  );
 
-  // チャンネル作成ボタンが押された時にモーダルを表示する
-  createChannelButton.addEventListener("click", () => {
-    createChannelModal.style.display = "flex";
-  });
+const createChannelModal = document.getElementById("create-channel-modal");
+const createChannelButton = document.getElementById("create-channel-button");
+const closeButton = document.getElementById(
+  "create-channel-modal-close-button"
+);
 
-  // モーダル内のXボタンが押されたときにモーダルを閉じる
-  closeButton.addEventListener("click", () => {
+// チャンネル作成ボタンが押された時にモーダルを表示する
+createChannelButton.addEventListener("click", () => {
+  createChannelModal.style.display = "flex";
+});
+
+// モーダル内のXボタンが押されたときにモーダルを閉じる
+closeButton.addEventListener("click", () => {
+  createChannelModal.style.display = "none";
+});
+
+// 画面のどこかが押された時にモーダルを閉じる
+addEventListener("click", (e) => {
+  if (e.target == createChannelModal) {
     createChannelModal.style.display = "none";
-  });
-
-  // 画面のどこかが押された時にモーダルを閉じる
-  addEventListener("click", (e) => {
-    if (e.target == createChannelModal) {
-      createChannelModal.style.display = "none";
-    }
-  });
-};
+  }
+});
